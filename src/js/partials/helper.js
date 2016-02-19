1020642a9edda4a7f1b32d584a38929fd7ae41f4
zorro76 <sergey.martynuk@gmail.com>
2016-02-19 21:41:00 +0200

    create accordion, add product&brand page

--- src/js/partials/helper.js
+++ src/js/partials/helper.js
@@ -34,12 +34,46 @@ $(document).ready(function(){
         infinite: false
     });
 
+//small slider
+
+    $('.portfolio-item-slider2').on('init', function(event, slick, currentSlide){
+        var nrCurrentSlide = slick.currentSlide + 1,
+            totalSlidesPerPage = nrCurrentSlide + 3;
+        $('.controls').html(nrCurrentSlide + " - " + totalSlidesPerPage + " of " + slick.slideCount);
+    });
+
+    $('.portfolio-thumb-slider2').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.portfolio-item-slider2',
+        dots: false,
+        arrows: true,
+        focusOnSelect: true,
+        infinite: false
+    });
+
+    $('.portfolio-item-slider2').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.portfolio-thumb-slider2',
+        infinite: false
+    });
+
     $('#myCarousel').carousel({
         arrows: true
     })
 });
 
-
+function toggleChevron(e) {
+    $(e.target)
+        .prev('.panel-heading')
+        .find('i.indicator')
+        .toggleClass('glyphicon-minus glyphicon-plus');
+    $('#accordion','.panel-heading').css('background-color', 'green');
+}
+$('#accordion').on('hidden.bs.collapse', toggleChevron);
+$('#accordion').on('shown.bs.collapse', toggleChevron);
 
 
 
